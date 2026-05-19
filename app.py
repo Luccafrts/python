3d2eb80dda227145de9481ae19a97fdaffbdf316
Lucca <luccafreitasleandro08@gmail.com>
2026-05-19 11:46:29 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route("/")
+def questao1():
+    return render_template("questao1.html", name ="pedro da silva me mimei camilo brum souza pinto cum nery alvez alvares cornelio anthony ansur gomide pereira braulio coelho do gordo paiva cachorro macaco marco cobra duarte braga oliveira perceval seleme proti susu andradas antonio carlos vargas muniz elias leite ")
 
-if __name__ == '__main__':
+@app.route("/questao2")
+def questao2():
+    dados = [{"nome": "lucca", "idade":17}]
+
+    return render_template("questao2.html", alunos=dados)
+    
+
+
+
+if __name__ == "__main__":
     app.run(debug=True)
